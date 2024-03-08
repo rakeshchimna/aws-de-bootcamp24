3e363841fb986c36369ee7f4e363e8488192e26a
Rakesh <162720781+rakeshchimna@users.noreply.github.com>
2024-03-08 21:30:54 +0530

    Update veg_order_dsm 050324.sql

--- veg_order_dsm.sql
+++ veg_order_dsm.sql
@@ -1,6 +1,12 @@
            SELECT   v_date,
                     s.br_code,
                     s.p_code,
+                    p.p_name,
+                    p.cat_type,
+                    p.cat_name,
+                    P.SUB_CAT_NAME,
+--                    GET_LAST_PUR_MRP(s.br_code,s.p_code) lst_pur_mrp,
+                    DECODE(SUBSTR(DOC_TYPE,1,1),'A','ADJ','SALE') DTYPE,
                     ROUND (SUM (DECODE (s.ri, 'I', inv_qty, 'R', -inv_qty))
                           ) sal_qty,
                     ROUND (SUM (DECODE (s.ri,
@@ -15,13 +21,20 @@
                                        )
                                )
                           ) gp_amt
-               FROM both_invkey s, product_master p
-              WHERE s.doc_type IN ('SO', 'SC', 'SR')
-                AND S.BR_CODE='SANK'
+               FROM both_invkey s, vu_product_master p
+              WHERE s.doc_type IN ('SO', 'SC', 'SR','AJ')
+--                AND S.BR_CODE='SANK'
                 AND s.p_code = p.p_code
                 and p.CAT_CODE = 13
                 AND s.v_date >= TRUNC (SYSDATE - 31)
                 AND s.v_date <= TRUNC (SYSDATE - 1)
               group by  s.p_code,
                     s.br_code,
-                    s.v_date                    
+                    s.p_code,
+                    p.p_name,
+                    s.v_date,
+                    p.cat_name,
+                    p.sub_cat_name,
+                    p.cat_type,
+                    DECODE(SUBSTR(DOC_TYPE,1,1),'A','ADJ','SALE')
+  
